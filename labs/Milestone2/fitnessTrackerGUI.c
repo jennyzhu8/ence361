5c8aa1f701de9cfb9d011e680fb313408e58240f
William Huang <whu35@uclive.ac.nz>
2022-05-04 23:13:03 +1200

    milestone2 mostly working

--- labs/Milestone2/fitnessTrackerGUI.c
+++ labs/Milestone2/fitnessTrackerGUI.c
@@ -51,10 +51,13 @@ typedef struct{
  * Constants
  **********************************************************/
 
-#define SYSTICK_RATE_HZ    10
-#define BUF_SIZE           10
+#define MAX_STR_LEN         32
+#define BUF_SIZE            10
 
-#define MAX_STR_LEN 32
+#define SYSTICK_RATE_HZ     25  // 25Hz results in 40ms resolution timer counter
+#define DISPLAY_PERIOD      5   // 200ms period = 5Hz
+#define BUTTON_PERIOD       1   // 40ms period = 25Hz
+#define ADC_PERIOD          2   // 80ms period = 12.5Hz
 
 //---USB Serial comms: UART0, Rx:PA0 , Tx:PA1
 #define BAUD_RATE 9600
@@ -74,7 +77,29 @@ static circBuf_t g_xAccBuffer;    // Buffer of size BUF_SIZE values of x acceler
 static circBuf_t g_yAccBuffer;    // Buffer of size BUF_SIZE values of y acceleration
 static circBuf_t g_zAccBuffer;    // Buffer of size BUF_SIZE values of z acceleration
 
-char statusStr[MAX_STR_LEN + 1];
+static circBuf_t g_goalBuffer;    // Buffer of size BUF_SIZE values read from potentiometer
+
+static uint32_t g_timerCount;     // Timer counter
+
+char statusStr[MAX_STR_LEN + 1];  // String for UART communication
+
+// Buttons states
+static uint8_t butStateUp;
+static uint8_t butStateDown;
+static uint8_t butStateLeft;
+static uint8_t butStateRight;
+static uint8_t buttonHoldTime;
+
+// State machine
+static int8_t displayState;
+static bool stepCountUnitToggle;
+static bool distTravelUnitToggle;
+static bool downButtonHold;
+
+// Step values
+static uint16_t stepCount;
+static uint16_t stepGoal;
+static int32_t goalBufferVal;
 
 /*******************************************
  * Local prototypes
@@ -108,6 +133,32 @@ SysTickIntHandler(void)
 
     // Poll button(s)
     updateButtons ();
+
+    // Increment timer
+    g_timerCount++;
+}
+
+//*****************************************************************************
+//
+// The handler for the ADC conversion complete interrupt.
+// Writes to a circular buffer.
+//
+//*****************************************************************************
+void
+ADCIntHandler(void)
+{
+    uint32_t ulValue;
+
+    //
+    // Get the single sample from ADC0.  ADC_BASE is defined in
+    // inc/hw_memmap.h
+    ADCSequenceDataGet(ADC0_BASE, 3, &ulValue);
+    //
+    // Place it in the circular buffer (advancing write index)
+    writeCircBuf (&g_goalBuffer, ulValue);
+    //
+    // Clean up, clearing the interrupt
+    ADCIntClear(ADC0_BASE, 3);
 }
 
 /***********************************************************
@@ -241,6 +292,37 @@ initialiseUSB_UART (void)
     UARTEnable(UART_USB_BASE);
 }
 
+/*********************************************************
+ * ADC (Potentiometer)
+ *********************************************************/
+void
+initADC (void)
+{
+    //
+    // The ADC0 peripheral must be enabled for configuration and use.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+
+    // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
+    // will do a single sample when the processor sends a signal to start the
+    // conversion.
+    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+
+    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH0 | ADC_CTL_IE |
+                             ADC_CTL_END);
+
+    //
+    // Since sample sequence 3 is now configured, it must be enabled.
+    ADCSequenceEnable(ADC0_BASE, 3);
+
+    //
+    // Register the interrupt handler
+    ADCIntRegister (ADC0_BASE, 3, ADCIntHandler);
+
+    //
+    // Enable interrupts for ADC0 sequence 3 (clears any outstanding interrupts)
+    ADCIntEnable(ADC0_BASE, 3);
+}
+
 /***********************************************************
  * Action functions
  ***********************************************************
@@ -316,114 +398,304 @@ UARTSend (char *pucBuffer)
 }
 
 /*********************************************************
+ * Convert a distance into a km string and display it
+ *********************************************************/
+void
+displayDistKM(uint16_t stepCount)
+{
+    char string[17];
+    int16_t distanceCM = stepCount * 90;
+    int16_t distanceKMD0 = distanceCM / 100000;
+    int16_t distanceKMM0 = distanceCM % 100000;
+    int16_t distanceKMD1 = distanceKMM0 / 10000;
+    int16_t distanceKMM1 = distanceKMM0 % 10000;
+    int16_t distanceKMD2 = distanceKMM1 / 1000;
+    usnprintf(string, sizeof(string), "%d.%d%d km", distanceKMD0, distanceKMD1, distanceKMD2);
+    OLEDStringDraw (string, 0, 1);
+}
+
+/*********************************************************
+ * Returns the average value in the goal buffer
+ *********************************************************/
+int32_t
+getGoalValue()
+{
+    uint16_t i;
+    int32_t sum = 0;
+    for (i = 0; i < BUF_SIZE; i++) {
+        sum = sum + readCircBuf (&g_goalBuffer);
+    }
+    int32_t mean = (2 * sum + BUF_SIZE) / 2 / BUF_SIZE;
+    int32_t value = mean * 10000 / 4095 / 100;
+    return (value*100);
+}
+
+/*********************************************************
+ * Convert a distance into a mile string and display it
+ *********************************************************/
+void
+displayDistMile(uint16_t stepCount)
+{
+    char string[17];
+    int16_t distanceCM = stepCount * 90;
+    int16_t distanceMD0 = distanceCM / 160934;
+    int16_t distanceMM0 = distanceCM % 160934;
+    int16_t distanceMD1 = distanceMM0 / 16093;
+    int16_t distanceMM1 = distanceMM0 % 16093;
+    int16_t distanceMD2 = distanceMM1 / 1609;
+    usnprintf(string, sizeof(string), "%d.%d%d miles", distanceMD0, distanceMD1, distanceMD2);
+    OLEDStringDraw (string, 0, 1);
+}
+
+/*********************************************************
+ * Convert steps to a percentage of steps goal
+ *********************************************************/
+void
+displayPercentGoal(uint16_t stepCount, uint16_t stepGoal)
+{
+    char string[17];
+
+    uint16_t wholeNum = 100 * stepCount / stepGoal;
+    uint16_t decimal = (100 * stepCount % stepGoal) / (stepGoal/10);
+
+    usnprintf(string, sizeof(string), "%d.%d%% of goal", wholeNum, decimal);
+    OLEDStringDraw (string, 0, 1);
+}
+
+/*********************************************************
+ * Display step count
+ *********************************************************/
+void
+displaySteps(uint16_t stepCount)
+{
+    char string[17];
+
+    usnprintf(string, sizeof(string), "%d steps", stepCount);
+    OLEDStringDraw (string, 0, 1);
+}
+
+/*********************************************************
+ * Display current step goal
+ *********************************************************/
+void
+displayCurrentGoal(uint16_t stepGoal)
+{
+    char string[17];
+
+    usnprintf(string, sizeof(string), "Current: %d", stepGoal);
+    OLEDStringDraw (string, 0, 1);
+}
+
+/*********************************************************
+ * Display new step goal
+ *********************************************************/
+void
+displayNewGoal(int32_t goalBufferVal)
+{
+    char string[17];
+
+    usnprintf(string, sizeof(string), "New: %d", goalBufferVal);
+    OLEDStringDraw (string, 0, 2);
+}
+
+/*********************************************************
+ * Get system time based on timer counter
+ *********************************************************/
+uint32_t
+getSysTime(void)
+{
+    uint32_t temp = 0;
+    IntMasterDisable();
+    temp = g_timerCount;
+    IntMasterEnable();
+    return temp;
+}
+
+/***********************************************************
+ * Tasks
+ ***********************************************************
+ * Display task
+ ***********************************************************/
+void
+displayTask(void)
+{
+    // Get goal buffer mean value
+    goalBufferVal = getGoalValue();
+
+    switch (displayState) {
+
+        case 0: // number of steps since reset
+            OLEDStringDraw ("                ", 0, 0);
+            OLEDStringDraw ("Steps Taken", 0, 0);
+            if (stepCountUnitToggle) {
+                OLEDStringDraw ("                ", 0, 1);
+                displayPercentGoal(stepCount, stepGoal);
+            }
+            else {
+                OLEDStringDraw ("                ", 0, 1);
+                displaySteps(stepCount);
+            }
+                        OLEDStringDraw ("                ", 0, 2);
+            break;
+
+        case 1: // distance travelled since reset
+            OLEDStringDraw ("                ", 0, 0);
+            OLEDStringDraw ("Travel Distance", 0, 0);
+            if (distTravelUnitToggle) {
+                OLEDStringDraw ("                ", 0, 1);
+                displayDistMile(stepCount);
+            }
+            else {
+                OLEDStringDraw ("                ", 0, 1);
+                displayDistKM(stepCount);
+            }
+            OLEDStringDraw ("                ", 0, 2);
+            break;
+
+        case 2: // set goal state
+            OLEDStringDraw ("                ", 0, 0);
+            OLEDStringDraw ("Set Goal", 0, 0);
+            OLEDStringDraw ("                ", 0, 1);
+            displayCurrentGoal(stepGoal);
+            OLEDStringDraw ("                ", 0, 2);
+            displayNewGoal(goalBufferVal);
+            break;
+    }
+
+    // long hold indicator bar
+    if (buttonHoldTime >= 100) {
+        OLEDStringDraw(":-) :-) :-) :-) ", 0, 3);
+    }
+    else if (buttonHoldTime >= 75) {
+        OLEDStringDraw(":-) :-) :-)     ", 0, 3);
+    }
+    else if (buttonHoldTime >= 50) {
+        OLEDStringDraw(":-) :-)         ", 0, 3);
+    }
+    else if (buttonHoldTime >= 25) {
+        OLEDStringDraw(":-)             ", 0, 3);
+    }
+    else {
+        OLEDStringDraw("                ", 0, 3);
+    }
+}
+
+/*********************************************************
+ * Manage the button presses
+ *********************************************************/
+void
+buttonTask(void)
+{
+    // Adjusting the display state with left and right button presses
+
+    butStateLeft = checkButton(LEFT);
+    butStateRight = checkButton(RIGHT);
+
+    if (butStateLeft == PUSHED) {
+        displayState++;
+        if (displayState > 2) {
+            displayState = 0;
+        }
+    }
+
+    if (butStateRight == PUSHED) {
+        displayState--;
+        if (displayState < 0) {
+            displayState = 2;
+        }
+    }
+
+    // Adjusting units with up button presses depending on the display state
+
+    butStateUp = checkButton(UP);
+
+    if (butStateUp == PUSHED) {
+        if (displayState == 0) {
+            stepCountUnitToggle = !stepCountUnitToggle;
+        }
+        else if (displayState == 1) {
+            distTravelUnitToggle = !distTravelUnitToggle;
+        }
+    }
+
+    // Down button press operations
+
+    butStateDown = checkButton(DOWN);
+
+    buttonHoldTime = 0;
+    if (butStateDown == PUSHED) {
+        downButtonHold = 1;
+        if (displayState == 2) {
+            stepGoal = goalBufferVal; //Setting the new goal
+            displayState = 0;
+        }
+    }
+    else if (downButtonHold && (butStateDown == NO_CHANGE)) {
+        buttonHoldTime++;
+    }
+    else {
+        buttonHoldTime = 0;
+        downButtonHold = 0;
+    }
+}
+
+/*********************************************************
  * Main
  *********************************************************/
 int
 main(void)
 {
-    uint8_t butStateUp;
-    uint8_t butStateDown;
-    uint8_t butStateLeft;
-    uint8_t butStateRight;
-
-    int8_t displayState = 0;
-
-    bool stepCountUnitToggle = 0;
-    bool distTravelUnitToggle = 0;
-
-    uint16_t stepCount = 15;
-    uint16_t stepGoal = 1000;
-    uint16_t goalPercentage = 100 * stepCount / stepGoal;
-
-    uint16_t distanceCM = stepCount * 90;
-
-    // Initialisation
+    // General initialisation
     initClock ();
     initDisplay ();
     initAccl ();
     initSysTick ();
     initButtons ();
+    initADC();
 
-    initCircBuf (&g_xAccBuffer, BUF_SIZE);
-    initCircBuf (&g_yAccBuffer, BUF_SIZE);
-    initCircBuf (&g_zAccBuffer, BUF_SIZE);
+    // Initialise circular buffer
+    initCircBuf (&g_goalBuffer, BUF_SIZE);
 
+    // Initialise USB UART
     initialiseUSB_UART ();
+
+    // Initialised state machine
+    displayState = 0;
+    stepCountUnitToggle = 0;
+    distTravelUnitToggle = 0;
+
+
+    // Initialise values
+    stepCount = 15;
+    stepGoal = 1000;
+
+    // Initialise task timers
+    uint32_t displayTaskTimer = 0;
+    uint32_t buttonTaskTimer = 0;
+    uint32_t adcTaskTimer = 0;
+    uint32_t currentTime;
 
     // Enable interrupts to the processor.
     IntMasterEnable();
 
     while (1)
     {
-        switch (displayState) {
+        currentTime = getSysTime();
 
-                case 0:
-                    OLEDStringDraw ("                ", 0, 0);
-                    OLEDStringDraw ("Steps Taken", 0, 0);
-                    if (stepCountUnitToggle) {
-                        displayUpdate ("% of goal:", "", goalPercentage, 1);
-                    }
-                    else {
-                        displayUpdate ("steps:", "", stepCount, 1);
-                    }
-                    OLEDStringDraw ("                ", 0, 2);
-                    break;
-                case 1:
-                    OLEDStringDraw ("                ", 0, 0);
-                    OLEDStringDraw ("Travel Distance", 0, 0);
-                    if (distTravelUnitToggle) {
-                        displayUpdate("miles:", "", (distanceCM / 160934), 1);
-                    }
-                    else {
-                        displayUpdate("km:", "", (distanceCM / 100000), 1);
-                    }
-                    OLEDStringDraw ("                ", 0, 2);
-                    break;
-                case 2:
-                    OLEDStringDraw ("                ", 0, 0);
-                    OLEDStringDraw ("Set Goal", 0, 0);
-                    displayUpdate("Current:", "", stepGoal, 1);
-                    displayUpdate("New:", "", 0, 2);
-                    break;
-                }
-
-
-
-        // Adjusting the display state with left and right button presses
-
-        butStateLeft = checkButton(LEFT);
-        butStateRight = checkButton(RIGHT);
-
-        if (butStateLeft == PUSHED) {
-            displayState++;
-            if (displayState > 2) {
-                displayState = 0;
-            }
+        if ((currentTime - displayTaskTimer) >= DISPLAY_PERIOD) {
+            displayTask();
+            displayTaskTimer = currentTime;
         }
 
-        if (butStateRight == PUSHED) {
-            displayState--;
-            if (displayState < 0) {
-                displayState = 2;
-            }
+        if ((currentTime - buttonTaskTimer) >= BUTTON_PERIOD) {
+            buttonTask();
+            buttonTaskTimer = currentTime;
         }
 
-        // Adjusting units with up button presses depending on the display state
-
-        butStateUp = checkButton(UP);
-
-        if (butStateUp == PUSHED) {
-            if (displayState == 0) {
-                stepCountUnitToggle = !stepCountUnitToggle;
-            }
-            else if (displayState == 1) {
-                distTravelUnitToggle = !distTravelUnitToggle;
-            }
+        if ((currentTime - adcTaskTimer) >= ADC_PERIOD) {
+            ADCProcessorTrigger(ADC0_BASE, 3);
+            adcTaskTimer = currentTime;
         }
 
-
-
-        // Delay for a period such that this loop runs at approximately 2Hz
-        SysCtlDelay (SysCtlClockGet () / 6);
     }
 }
